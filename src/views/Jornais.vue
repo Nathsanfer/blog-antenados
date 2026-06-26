28863afa21f913cff205b2dbdf28f3aa2b5ec48b
NathSanFer <158168917+Nathsanfer@users.noreply.github.com>
2026-06-25 22:03:02 -0300

    feat: create the gallery screen and fetches data from the database

--- src/views/Jornais.vue
+++ src/views/Jornais.vue
@@ -1,16 +1,81 @@
 <script>
-import HeaderTemplate from '../components/HeaderTemplate.vue';
+import HeaderTemplate from "../components/HeaderTemplate.vue";
 
 export default {
-  name: 'Jornais',
+  name: "Jornais",
   components: {
     HeaderTemplate,
   },
 };
-
 </script>
 
 <template>
-    <HeaderTemplate />
+  <HeaderTemplate />
+
+  <div class="header-page">
+    <img
+      src="../assets/icons_highlights/icon9.png"
+      alt="Icone de Beca"
+      class="icon-desktop"
+    />
+    <img src="../assets/icons_highlights/icon10.png" alt="Icone de Lâmpada" />
+    <div class="divisor"></div>
     <h1>Jornais</h1>
+    <div class="divisor"></div>
+    <img src="../assets/icons_highlights/icon11.png" alt="Icone de Livro" />
+    <img
+      src="../assets/icons_highlights/icon12.png"
+      alt="Icone de Colmeia"
+      class="icon-desktop"
+    />
+  </div>
+
+  <main>
+    <h1>Jornais</h1>
+    <h2>Em breve, você poderá acessar os jornais do IFSP</h2>
+  </main>
+
 </template>
+
+<style scoped>
+.header-page {
+  display: flex;
+  max-width: 1200px;
+  width: 90%;
+  margin: 1rem auto;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
+
+.header-page img {
+  width: 48px;
+  height: 48px;
+  flex-shrink: 0;
+}
+
+.divisor {
+  flex: 1;
+  height: 1px;
+  background-color: #a8a8a8;
+}
+
+.header-page h1 {
+  font-size: 30px;
+  font-weight: 500;
+  font-family: var(--secondary-font);
+  white-space: nowrap;
+}
+
+main {
+  display: flex;
+  margin: 0 auto;
+  flex-direction:  column;
+  max-width: 1200px;
+  width: 90%;
+  margin: 2rem auto 4rem;
+  gap: 2rem;
+  overflow-x: hidden;
+  background-color: #000;
+}
+</style>
